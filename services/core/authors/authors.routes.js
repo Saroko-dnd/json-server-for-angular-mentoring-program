90242adfdf135fc876fe1b6a0ca9e12d05b878f7
Ihar Saroka <Ihar_Saroka@epam.com>
2019-02-19 11:25:23 +0300

    master: added authors json db

--- services/core/authors/authors.routes.js
+++ services/core/authors/authors.routes.js
@@ -4,9 +4,7 @@ const jsonServer = require('json-server');
 const router = express.Router();
 
 router.use(jsonServer.rewriter({
-    '/courses': '/courses',
-    '/courses/number': '/courses/number',
-	'/courses/:id': '/courses/:id',
+	'/authors': '/authors',
 }));
 
 module.exports = router;
--- services/core/courses/courses.middleware.js
+++ services/core/courses/courses.middleware.js
@@ -2,39 +2,34 @@ const express = require('express');
 const router = express.Router();
 const url = require('url');
 
-module.exports = server => {
-    router.get('/courses', (req, res, next) => {
-        let url_parts = url.parse(req.originalUrl, true),
-            query = url_parts.query,
-            from = query.start || 0,
-            to = +query.start + +query.count,
-            sort = query.sort,
-            queryStr = query.query,
-            courses = server.db.getState().courses;
+module.exports = (server) => {
 
-        if (!!query.textFragment) {
-            courses = courses.filter(
-                course =>
-                    course.name
-                        .concat(course.description)
-                        .toUpperCase()
-                        .indexOf(query.textFragment.toUpperCase()) >= 0
-            );
-        }
+	router.get('/courses', (req, res, next) => {
+		let url_parts = url.parse(req.originalUrl, true),
+			query = url_parts.query,
+			from = query.start || 0,
+			to = +query.start + +query.count,
+			sort = query.sort,
+			queryStr = query.query,
+			courses = server.db.getState().courses;
+		
+			if (!!query.textFragment) {
+				courses = courses.filter((course) => course.name.concat(course.description).toUpperCase().indexOf(query.textFragment.toUpperCase()) >= 0);
+			}
 
-        if (courses.length < to || !to) {
-            to = courses.length;
-        }
-        courses = courses.slice(from, to);
-
-        res.json(courses);
+		if (courses.length < to || !to) {
+			to = courses.length;
+		}
+		courses = courses.slice(from, to);
+		
+		res.json(courses);
     });
-
+    
     router.get('/courses/number', (req, res, next) => {
         let courses = server.db.getState().courses;
 
         res.json({ numberOfCourses: courses.length });
     });
-
-    return router;
+	
+	return router;
 };
